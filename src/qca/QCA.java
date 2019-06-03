13d4dfc0f9c65c5b44bc0e329b1807f75175717d
francoing <franco.montti.19@gmail.com>
2019-06-03 00:50:07 -0300

    primera subida

---
+++ src/qca/QCA.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package qca;
+import controlador.Controlador;
+/**
+ *
+ * @author El´s
+ */
+public class QCA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          Controlador c=new Controlador();
+          c.ejecutar();   
+    }
+    
+}
